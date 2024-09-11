b83e818a414dfab115fc5f4ec356c55df11cfc97
Mustafa Aamir <mustafa.290101@gmail.com>
2024-09-11 22:05:19 +0400

    added measure

--- quantipy.py
+++ quantipy.py
@@ -1,10 +1,18 @@
-import math
 import random
+import cmath
+from collections import Counter
+import matplotlib.pyplot as plt
 
 
-class quantumCircuit:
+class QuantumCircuit:
     def __init__(self,n=1):
-        self.qubits=[[1,0] for i in range(n)]
+        self.qubits=[[1,0] for _ in range(n)]
+
+    def __repr__(self):
+        str_qubits = ""
+        for i, qubit in enumerate(self.qubits):
+            str_qubits += f"Quibit {i}: [{qubit[0]}, {qubit[1]}]\n"
+        return str_qubits
 
     def PaulliX(self,i):
         self.qubits[i]=[self.qubits[i][1],self.qubits[i][0]]
@@ -15,20 +23,29 @@ class quantumCircuit:
     def PaulliZ(self,i):
         self.qubits[i]=[self.qubits[i][0],self.qubits[i][1]*-1]
 
-    def PHASE(self,i, theta):
-        self.qubits[i]=[self.qubits[i][0],self.qubits[i][1]*((-1)**(theta/math.pi))]
 
-    def SWAP(self,i,j,):
+    def PHASE(self,i, theta):
+        self.qubits[i].superposition=[
+                self.qubits[i][0],
+                complex(self.qubits[i][1]*(cmath.exp(1j * theta)))
+                ]
+        if (theta % cmath.pi == 0):
+            self.qubits[i][1] = self.qubits[i][1].real
+
+    def SWAP(self, i, j):
         temp=self.qubits[i]
         self.qubits[i]=self.qubits[j]
         self.qubits[j]=temp
 
-    def CNOT(self,i,j):
+    def CNOT(self, i, j):
         if self.qubits[i]==[0,1]:
             self.PaulliX(self.qubits[j])
 
     def HADAMARD(self,i):
-        self.qubits[i]=[(1/(2**0.5))*(self.qubits[i][1]+self.qubits[i][0]),(1/(2**0.5))*((-1*self.qubits[i][1])+self.qubits[i][0])]
+        self.qubits[i]=[
+                (1/(2**0.5))*(self.qubits[i][1]+self.qubits[i][0]),
+                (1/(2**0.5))*((-1*self.qubits[i][1])+self.qubits[i][0])
+                ]
 
     def CSWAP(self,i,j,k):
         if self.qubits[i]==[0,1]:
@@ -54,4 +71,40 @@ class quantumCircuit:
             ret = 1
             self.qubits[i]= [0, 1]
         return ret
-    
+
+
+
+def quantum_rng(bits):
+    qc = QuantumCircuit(bits)
+    number = 0
+    for i in range(bits):
+        qc.HADAMARD(i)
+        result = qc.measure(i)
+        number |= (result << i)
+    return number
+
+
+def random_plot():
+    qc = QuantumCircuit(10)
+    for i in range(10):
+        qc.HADAMARD(i)
+        print(qc.measure(i))
+
+    # Generate a 8-bit random number
+    random_number = quantum_rng(8)
+    print(f"8-bit Quantum Random Number: {random_number} (binary: {random_number:08b})")
+
+    # Generate multiple 8-bit random numbers
+    num_samples = 1000
+    samples = [quantum_rng(8) for _ in range(num_samples)]
+    counts = Counter(samples)
+    plt.bar(list(counts.keys()), list(counts.values()))
+    plt.title("Distribution of Quantum Random Numbers")
+    plt.xlabel("Number")
+    plt.ylabel("Frequency")
+    plt.show()
+
+    mean = sum(samples) / num_samples
+    variance = sum((x - mean) ** 2 for x in samples) / num_samples
+    print(f"Mean: {mean:.2f}")
+    print(f"Variance: {variance:.2f}")
