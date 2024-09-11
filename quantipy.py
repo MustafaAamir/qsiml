7cd2d4916cc7f7513dabad8ea0a790e36088574d
CodeSkull <yusuff.0279@gmail.com>
2024-09-11 21:43:26 +0400

    measure function

--- quantipy.py
+++ quantipy.py
@@ -1,4 +1,6 @@
 import math
+import random
+
 
 class quantumCircuit:
     def __init__(self,n=1):
@@ -14,7 +16,7 @@ class quantumCircuit:
         self.qubits[i]=[self.qubits[i][0],self.qubits[i][1]*-1]
 
     def PHASE(self,i, theta):
-        self.qubits[i]=[self.qubits[i][0],self.qubits[i][1]*(-1**(theta/math.pi))]
+        self.qubits[i]=[self.qubits[i][0],self.qubits[i][1]*((-1)**(theta/math.pi))]
 
     def SWAP(self,i,j,):
         temp=self.qubits[i]
@@ -35,3 +37,20 @@ class quantumCircuit:
     def CCNOT(self,i,j,k):
         if self.qubits[i]==[0,1] and self.qubits[j]==[0,1]:
             self.PaulliX(k)
+    def measure(self, i):
+        pzero = self.qubits[i][0].real ** 2
+        pone  = self.qubits[i][1].real ** 2
+        # total probability should be one i think
+
+        tp = pzero + pone
+        pzero = pzero / tp
+        pone  = pone / tp
+
+        random_float = random.random()
+        if random_float < pzero:
+            ret = 0
+            self.qubits[i] = [1, 0]
+        else:
+            ret = 1
+            self.qubits[i]= [0, 1]
+        return ret
