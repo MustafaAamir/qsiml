89a4de777374a0715162efa65bc8abf1c685623c
CodeSkull <yusuff.0279@gmail.com>
2024-09-12 22:07:31 +0400

    paulli test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 from quantipy import QuantumCircuit
-import math
+import cmath
 
 HALF_SQRT = complex((1/2) ** 0.5)
 COMPLEX_ZERO = complex(0)
@@ -24,19 +24,50 @@ class TestQuantipy(unittest.TestCase):
         self.assertAlmostEqual(zeroeth, qc.qubits[0][0])
         self.assertAlmostEqual(oneth, qc.qubits[0][1])
 
+    def test_paulliX(self):
+        qc = QuantumCircuit(1)
+
+        qc.PaulliX(0)
+        self.assertAlmostEqual(0, qc.qubits[0][0])
+        self.assertAlmostEqual(1, qc.qubits[0][1])
+        qc.PaulliX(0)
+        self.assertAlmostEqual(1, qc.qubits[0][0])
+        self.assertAlmostEqual(0, qc.qubits[0][1])
+
+    def test_paulliY(self):
+        qc = QuantumCircuit(2)
+        qc.PaulliX(1)
+        qc.PaulliY(0)
+        self.assertAlmostEqual(0, qc.qubits[0][0])
+        self.assertAlmostEqual(1j, qc.qubits[0][1])
+        qc.PaulliY(1)
+        self.assertAlmostEqual(-1j, qc.qubits[1][0])
+        self.assertAlmostEqual(0, qc.qubits[1][1])
+
+    def test_paulliZ(self):
+        qc = QuantumCircuit(2)
+        qc.PaulliX(1)
+        qc.PaulliZ(0)
+        self.assertAlmostEqual(1, qc.qubits[0][0])
+        self.assertAlmostEqual(0, qc.qubits[0][1])
+        qc.PaulliZ(1)
+        self.assertAlmostEqual(0, qc.qubits[1][0])
+        self.assertAlmostEqual(-1, qc.qubits[1][1])
+
+
     def test_PHASE_noPauliX(self):
         qc = QuantumCircuit(5)
-        pi_values = [0.0, math.pi / 2, math.pi, (1.5) * math.pi, 2 * math.pi]
+        pi_values = [0.0, cmath.pi / 2, cmath.pi, (1.5) * cmath.pi, 2 * cmath.pi]
         for i in range(5):
             qc.PHASE(i, pi_values[i])
             self.assertAlmostEqual(qc.qubits[i][1], COMPLEX_ZERO)
 
     def test_PHASE_PauliX(self):
         qc = QuantumCircuit(5)
-        pi_values = [0.0, math.pi / 2, math.pi, (1.5) * math.pi, 2 * math.pi]
+        pi_values = [0.0, cmath.pi / 2, cmath.pi, (1.5) * cmath.pi, 2 * cmath.pi]
         # i added some changes to PHASE
         # if theta % pi then use the real part only (-1 or 1)
-        # else if theta % math.pi/2 then use the imaginary part only (-1j or 1j)
+        # else if theta % cmath.pi/2 then use the imaginary part only (-1j or 1j)
 
         euler_values = [COMPLEX_ONE, complex(0 + 1j), -COMPLEX_ONE, complex(0 - 1j), COMPLEX_ONE]
         for i in range(5):
@@ -45,6 +76,7 @@ class TestQuantipy(unittest.TestCase):
             self.assertAlmostEqual(qc.qubits[i][1].real, euler_values[i].real)
 
 
-
+Testing=TestQuantipy()
+Testing.test_paulliX()
 
 
